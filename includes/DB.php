882d3965250ed2d3f1ee31c74e241eabbf088660
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2018-08-24 14:05:04 +0100

    Update DB.php

--- includes/DB.php
+++ includes/DB.php
@@ -1,11 +1,10 @@
 <?php 
-$connection = mysql_connect("localhost","root","");
-$conection_DB = mysql_select_db("culrural_project",$connection);
-if($connection_DB){
-    echo 'connected';
-}
-else{
-    echo 'not connected'
-}
-
+	$connection = mysql_connect("localhost","root","");
+	$connection_DB = mysql_select_db("culture_project",$connection);
+	if($connection_DB){
+    	echo 'connected';
+	}
+	else{
+    	echo 'not connected';
+	}
 ?>
