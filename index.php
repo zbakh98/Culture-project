99c5e1f5f63cb6ac0e3028892baa2a4bb4c08058
achrafxx <42671332+achrafxx@users.noreply.github.com>
2018-08-24 12:09:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/Publicstyles.css">
+</head>
+<body>
+    <div class="container">  
+        <div class="row justify-content-sm-center">  
+            <div id="form" class="col-sm-6">
+                <p class="title">Welcome to Culture competition</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Fusce iaculis dui nec mauris efficitur elementum. Pelle
+                    ntesque condimentum viverra ipsum. Mauris sollicitudin 
+                    pellentesque molestie. Etiam risus sapien, aliquet id r
+                    honcus eget, faucibus vel orci. Donec ullamcorper ornar
+                    e ligula vel scelerisque. Pellentesque dapibus, augue ne
+                    c aliquam ultrices, risus elit ultrices libero, vitae con
+                    sequat ligula dui id ligula. Vivamus quis lacus a lectus c
+                    ursus tristique. Morbi dolor ante, dignissim vitae maximus 
+                    a, ultrices vitae dolor. Nulla sed commodo quam. Ut vitae fermentum nibh. 
+                    Vivamus consectetur leo eu erat euismod porta. Integer fermentum ligula 
+                    consectetur finibus lobortis.</p>
+                <button class="form-control btn btn-success" href="login.php">Continue</button>    
+            </div>
+        </div>      
+    </div>      
+</body>
+
+</html>
