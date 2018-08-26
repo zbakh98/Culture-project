8e191700270b504dfc7c38f593f59f0bd33b1175
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2018-08-26 19:39:46 +0100

    Add files via upload

---
+++ finish.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/Publicstyles.css">
+</head>
+<body>
+    <div class="container">  
+        <div class="row justify-content-sm-center">  
+            <div id="form" class="col-sm-6">
+                <p class="title">Attendez le signal de debut</p>
+                
+                <a class="form-control btn btn-success" href="formulaire.php">debut</a>    
+            </div>
+        </div>      
+    </div>      
+</body>
+
+</html>
---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+// Suppression des variables de session et de la session
+$_SESSION = array();
+session_destroy();
+// Suppression des cookies de connexion automatique
+
+header('Refresh: 0;url=login.php');
+?>
