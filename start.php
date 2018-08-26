f377d3c1ed40256030a19f0dd398b6144abbf326
zbakh98 <42416143+zbakh98@users.noreply.github.com>
2018-08-26 19:38:24 +0100

    Update start.php

--- start.php
+++ start.php
@@ -14,35 +14,60 @@
 		include("includes/DB.php");
 		global $connection_DB;
 		$id=$_SESSION['id'];
-		$sql = "SELECT * FROM parrains WHERE id='$id'";
+		$p_id=$_SESSION['p_id'];
+		$sql = "SELECT * FROM equipe WHERE p_id='$p_id'";
 		$result = mysql_query($sql);
 		$data=mysql_fetch_array($result);
-		if($data['test']==0)
+		if ($data == 0) 
 		{
-	?>
-	<div class="container">  
-        <div class="row justify-content-sm-center">  
-            <div id="form" class="col-sm-6">
-                <p class="title">Wait For The Signal</p>
-                
-                <a class="form-control btn btn-success" href="formulaire.php">start</a>    
-            </div>
-        </div>      
-    </div>
-    <?php
-    	}
-    	else
-    	{
-    ?>
-	<div class="container">  
-        <div class="row justify-content-sm-center">  
-            <div id="form" class="col-sm-6">
-                <p class="title">sorry you're out</p>   
-            </div>
-        </div>      
-    </div>
-    <?php
-    	}
-    ?>
+			if(isset($_POST['submit']))
+			{
+				$name = mysql_real_escape_string($_POST["name"]);
+				$namep = mysql_real_escape_string($_POST["namep"]);
+				$np = mysql_real_escape_string($_POST["np"]);
+				$Query = "INSERT INTO equipe(p_id, team_name, p_name, n_participants) VALUES ('$p_id', '$name', '$namep', '$np')";
+				$Execute = mysql_query($Query);
+				if($Execute)
+				{
+					header('Refresh: 0;url=finish.php');
+				}
+				else
+				{
+					echo 'ERREUR';
+				} 
+			}
+				?>
+				<div class="container">  
+			        <div class="row justify-content-sm-center">  
+			            <div id="form" class="col-sm-6">
+			                	<form action="start.php" method="post">
+			                		<div class="form-group">
+									    <label for="pseudo">nom parrain</label>
+									    <input name="namep" type="pseudo" class="form-control" id="namep" placeholder="entrer le nom du parrain">
+									</div>
+								  	<div class="form-group">
+									    <label for="pseudo">nom d'equipe</label>
+									    <input name="name" type="pseudo" class="form-control" id="name" placeholder="entrer le nom d'equipe">
+									</div>
+									<div class="form-group">
+									    <label for="pseudo">nombre de participants</label>
+									    <input name="np" type="pseudo" class="form-control" id="np" placeholder="entrer le nombre de participants">
+									</div>
+								  	<button type="submit" class="btn btn-success form-control" name="submit">Submit</button>
+								</form>
+			            </div>
+			        </div>      
+			    </div><?php
+	    }	
+	    else
+	    { ?>
+	    		<div class="container">  
+			        <div class="row justify-content-sm-center">  
+			            <div id="form" class="col-sm-6">
+			                <p>Vous etes deja inscrits aller a <a href="finish.php"> la page de debut </a></p>
+			            </div>
+			        </div>      
+			    </div>
+	    <?php }	?>
 </body>
 </html>
